0460bc9eb80df178ec390b5088465a9847524c06
lettersmatter <lakeness@ya.ru>
2026-08-26 22:48:40 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,10 @@ int main() {
 		std::cout << "Enter device address: ";
 		std::cin >> address;
 	} while(!m.selectDevice(address));
-
 	std::cout << "Current backlight level: " << (int)m.getBackLightLevel() << std::endl;
-
 	do {
 		std::cout << "Select backlight level(0-3): ";
 		std::cin >> level;
 	} while(m.setBackLightLevel(level) < 0);
-
 	return 0;
 }
